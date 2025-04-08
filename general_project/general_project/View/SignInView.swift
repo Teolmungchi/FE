6f5bb09fc678e135b9c46ec675fea19a0571fd29
sangyup <ideallyup@naver.com>
2025-04-08 19:35:42 +0900

    edit root view

--- general_project/general_project/View/SignInView.swift
+++ general_project/general_project/View/SignInView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct LoginView: View {
+struct SignInView: View {
     @StateObject private var viewModel = SignInViewModel()
 
     
@@ -69,5 +69,5 @@ struct LoginView: View {
     }
 }
 #Preview{
-    LoginView()
+    SignInView()
 }
